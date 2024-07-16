969acf9d3603b6ba7b6262004c91667730d9a501
unknown <bipronilghosh@gmail.com>
2024-07-16 17:58:30 +0530

    first commit

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -10,9 +10,9 @@ const Logo = () => {
     <Image
       onClick={() => router.push('/')}
       className="hidden md:block cursor-pointer" 
-      src="/images/logo.png" 
-      height="100" 
-      width="100" 
+      src="/images/logo.jpeg" 
+      height="60" 
+      width="60" 
       alt="Logo" 
     />
    );
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ import ClientOnly from './components/ClientOnly';
 import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
+  title: 'HomeHaven',
+  description: 'HomeHaven',
 }
 
 const font = Nunito({ 
